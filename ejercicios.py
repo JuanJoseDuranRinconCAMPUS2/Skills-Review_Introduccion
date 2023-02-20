e64974f51d84ffb9259e98c2f1e73d7704c5d977
JuanJoseDuranRinconCAMPUS2 <125879491+JuanJoseDuranRinconCAMPUS2@users.noreply.github.com>
2023-02-20 18:06:52 -0500

    ejercicio3

--- ejercicios.py
+++ ejercicios.py
@@ -1,13 +1,9 @@
-
 from colorama import init, Fore, Back, Style
 
-""" 2. En la jerarquía de operadores, cuáles son los que más
-prioridad tienen cuando el intérprete de Python los evalúa? """
+""" 3. Si hay operadores de igual precedencia, en qué orden se
+ejecutan?
 
+A. De izquierda a derecha
+B. De derecha a izquierda """
 
-
-print(Fore.GREEN+" \t La jerarquía es la siguiente")
-print(" \t Operaciones en Parentesis--------()-------- \n\t Potencias--------**-------- \n\t Multiplicacion y Divisiones--------+,%,/,//-------- \n\t Suma y Resta-------- +- --------")
-print("\t Operadores Relacionales--------<,=,>--------  \n\t Operador Logico And--------and-------- \n\t Operador Logico Or--------or--------")
-
-
+print("La respuesta es A")
