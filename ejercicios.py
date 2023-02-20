17694a4f3a6950486421e6c7b4d23a6ec95392ea
JuanJoseDuranRinconCAMPUS2 <125879491+JuanJoseDuranRinconCAMPUS2@users.noreply.github.com>
2023-02-20 18:12:05 -0500

    ejercicio10

--- ejercicios.py
+++ ejercicios.py
@@ -1,22 +1,29 @@
-from colorama import init, Fore, Back, Style
 
-""" 9. Construya un algoritmo en Python, que permita ingresar la
-información de un empleado e imprima el nombre, los
-apellidos y la antigüedad. Los datos que se deben solicitar
-son los siguientes:
-*Nombre * Teléfono *Año de ingreso a la empresa
-*Apellidos *Edad. """
 
-def Datos():
-    actual= int(2023)
-    print(Fore.MAGENTA+"Hola BIenvenido, Ingresa tus datos Por Favor ;3")
-    Nombre=str(input(Fore.BLUE+"Ingresa tu nombre completo sin apellidos <"))
-    Apellidos=str(input(Fore.BLUE+"Ingresa tus Apellidos <"))
-    Edad=int(input(Fore.BLUE+"Ingresa tu Edad <"))
-    AñoDeImgreso=int(input(Fore.BLUE+"Ingresa el año que ingresaste a nuestra empresa <"))
-    permiso=int(input("¿Quieres ver tus datos mas importantes?   1.Si 2.No <"))
-    if permiso == 1:
-        print(Fore.GREEN+"Tus Datos de usuario son: \n Nombre y Apellidos:" , Nombre, Apellidos, "\nAño de ingreso a nuestra empresa:", AñoDeImgreso, "\n Tiempo en la empresa:", {actual-AñoDeImgreso})
-    return(Nombre, Apellidos, Edad, AñoDeImgreso)
+""" 10. En su casa le solicitan que realice un algoritmo en Python,
+que permita calcular el valor a pagar por concepto de
+energía eléctrica. Los datos que se conocen son los
+siguientes:
+- Mes de consumo - Valor kw
+-Total kw consumido en el mes - estrato """
+
+def HACIENDOELTRABAJODEESSA ():
+    ValorKW= 315.6
+    print("\tBienvenido a la calculadora de deudas con ESSA :3. Por favor ingresar los siguientes datos:")
+    MesdeConsumo = str(input("Ingresa el mes de la factura  <"))
+    Estrato = int(input("Ingresa el estrato que perteneces  <"))
+    TotalKw = int(input("Ingresa el total de  Kw consumidos en el mes  <"))
+    valorTotal= ValorKW*TotalKw
+    if Estrato == 1:
+        descuento=valorTotal*0.6
+    if Estrato == 2:
+        descuento=valorTotal*0.5
+    if Estrato == 3:
+        descuento=valorTotal*0.15
+        
+    print("El valor total a pagar por la electricidad consumida en", MesdeConsumo , "\n segun el estrato registrado" , (Estrato) ,"es", valorTotal-descuento )
     
-Datos()
+    return()
+    
+    
+HACIENDOELTRABAJODEESSA()
