af9c46433ee0fc71f2bda7043b07d4f8b44b4e2b
JuanJoseDuranRinconCAMPUS2 <125879491+JuanJoseDuranRinconCAMPUS2@users.noreply.github.com>
2023-02-20 18:06:12 -0500

    ejercicio2

--- ejercicios.py
+++ ejercicios.py
@@ -1,13 +1,13 @@
 
-"""1. Qué operadores utiliza Python en los siguientes casos:
+from colorama import init, Fore, Back, Style
 
-A. División Modular
-B. Exponenciación
-C. División que retorne entero."""
+""" 2. En la jerarquía de operadores, cuáles son los que más
+prioridad tienen cuando el intérprete de Python los evalúa? """
 
-num1 = 10
-num2 = 2
 
-print("B. Exponenciación {num1}**{num2} =", num1**num2 )
-print("A. División Modular {num1}%{num2} =", num1%num2)
-print("C. División que retorne entero {num1}//{num2} =", num1//num2)
+
+print(Fore.GREEN+" \t La jerarquía es la siguiente")
+print(" \t Operaciones en Parentesis--------()-------- \n\t Potencias--------**-------- \n\t Multiplicacion y Divisiones--------+,%,/,//-------- \n\t Suma y Resta-------- +- --------")
+print("\t Operadores Relacionales--------<,=,>--------  \n\t Operador Logico And--------and-------- \n\t Operador Logico Or--------or--------")
+
+
