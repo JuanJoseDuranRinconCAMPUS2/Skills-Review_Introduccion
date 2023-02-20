624fc13634dcbe54e179ecbe2c05693b86e92ae1
JuanJoseDuranRinconCAMPUS2 <125879491+JuanJoseDuranRinconCAMPUS2@users.noreply.github.com>
2023-02-20 18:10:51 -0500

    ejercicio9

--- ejercicios.py
+++ ejercicios.py
@@ -1,19 +1,22 @@
 from colorama import init, Fore, Back, Style
 
-""" 8. Escriba un bloque cualquiera de código en Python en donde
-utilice 2 condicionales (if) anidados. """
+""" 9. Construya un algoritmo en Python, que permita ingresar la
+información de un empleado e imprima el nombre, los
+apellidos y la antigüedad. Los datos que se deben solicitar
+son los siguientes:
+*Nombre * Teléfono *Año de ingreso a la empresa
+*Apellidos *Edad. """
 
-numero=input(Fore.MAGENTA+"Ingresa un numero plis :3      ")
-numero=int(numero)
-
-if int(numero) == numero:
-    if numero %2== 0:
-        if numero >20:
-            print("El dato es un numero par y es mayor o igual a 20")
-        else:
-            print("El dato es un numero par menor que 20")
-    else:
-        print("El dato es un numero y es impar")
-
-else:
-    print("te dije que digitaras un numero >:c")
+def Datos():
+    actual= int(2023)
+    print(Fore.MAGENTA+"Hola BIenvenido, Ingresa tus datos Por Favor ;3")
+    Nombre=str(input(Fore.BLUE+"Ingresa tu nombre completo sin apellidos <"))
+    Apellidos=str(input(Fore.BLUE+"Ingresa tus Apellidos <"))
+    Edad=int(input(Fore.BLUE+"Ingresa tu Edad <"))
+    AñoDeImgreso=int(input(Fore.BLUE+"Ingresa el año que ingresaste a nuestra empresa <"))
+    permiso=int(input("¿Quieres ver tus datos mas importantes?   1.Si 2.No <"))
+    if permiso == 1:
+        print(Fore.GREEN+"Tus Datos de usuario son: \n Nombre y Apellidos:" , Nombre, Apellidos, "\nAño de ingreso a nuestra empresa:", AñoDeImgreso, "\n Tiempo en la empresa:", {actual-AñoDeImgreso})
+    return(Nombre, Apellidos, Edad, AñoDeImgreso)
+    
+Datos()
