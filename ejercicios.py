618a66242b5f42da099a09a0288c2a526bdbc75b
JuanJoseDuranRinconCAMPUS2 <125879491+JuanJoseDuranRinconCAMPUS2@users.noreply.github.com>
2023-02-20 18:08:53 -0500

    ejercicio6

--- ejercicios.py
+++ ejercicios.py
@@ -1,15 +1,7 @@
 from colorama import init, Fore, Back, Style
 
-""" 5. Enumere 5 tipos de datos en Python y suministre un valor de
-ejemplo de cada uno. """
-numeros=(1,2,3,4,5)
-Str= "Hello World"
-Int= 4
-Float = 0.5
-Bool= True
-list=["hola", 23, "programador graduado de plazit :3"]
-print(numeros[0] , Fore.MAGENTA+"STR(Texto)ejemplo =" , Str)
-print(numeros[1] , Fore.BLUE+"Int(Numero entero) ejemplo =" , Int)
-print(numeros[2] , Fore.RED+"Int(Numero Decimal) ejemplo =" , Float)
-print(numeros[3] , Fore.YELLOW+"Bool(Booleano) ejemplo =" , Bool)
-print(numeros[4] , Fore.GREEN+"List(lista) ejemplo =" , list)
+""" 6. En sus propias palabras, qué son las funciones
+preconstruidas y proporcione 2 ejemplos. """
+
+print(Fore.MAGENTA+"Son aquellas funciones que vienen por defecto en python, las cuales tienen la funcion de realizar tareas simples \n y muy utiles a la hora de programar, se puede definir como las funciones que hacen mas facil la vida del programador :3")
+print(Fore.BLUE+"ejemplo =  print (“Hola”) y float(‘2.22’)")
