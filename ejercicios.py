ecefa5b0b1b8281cf0d3a1d92e3170d230d07f37
JuanJoseDuranRinconCAMPUS2 <125879491+JuanJoseDuranRinconCAMPUS2@users.noreply.github.com>
2023-02-20 18:09:55 -0500

    ejercicio7

--- ejercicios.py
+++ ejercicios.py
@@ -1,7 +1,6 @@
 from colorama import init, Fore, Back, Style
 
-""" 6. En sus propias palabras, qué son las funciones
-preconstruidas y proporcione 2 ejemplos. """
+""" 7. Cuál es la diferencia entre un condicional simple y un
+condicional compuesto? """
 
-print(Fore.MAGENTA+"Son aquellas funciones que vienen por defecto en python, las cuales tienen la funcion de realizar tareas simples \n y muy utiles a la hora de programar, se puede definir como las funciones que hacen mas facil la vida del programador :3")
-print(Fore.BLUE+"ejemplo =  print (“Hola”) y float(‘2.22’)")
+print(Fore.YELLOW+"La diferencia es la cantidad de rutas que tenemos en una o en otra, en la condicional simple solo se puede realizar una accion o no hacerla \n dando como resultado un unico resultado, mientras que en en la compuesta hay varias acciones las cuales llevan a un mismo resultado pero con diferente ruta de procedimiento")
