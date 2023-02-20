e4ad12232c7256f06b345055e73f760cb5312460
JuanJoseDuranRinconCAMPUS2 <125879491+JuanJoseDuranRinconCAMPUS2@users.noreply.github.com>
2023-02-20 18:07:21 -0500

    ejercicio4

--- ejercicios.py
+++ ejercicios.py
@@ -1,9 +1,11 @@
 from colorama import init, Fore, Back, Style
+from PIL import Image
 
-""" 3. Si hay operadores de igual precedencia, en qué orden se
-ejecutan?
 
-A. De izquierda a derecha
-B. De derecha a izquierda """
+""" 4. Que son las expresiones regulares en Python? """
 
-print("La respuesta es A")
+print(Fore.MAGENTA+" \t son expresiones comodín que definen patrones de caracteres a emparejar y extraer de una cadena de texto por ejemplo tenemos:")
+print(Fore.CYAN+" \t . ^ $ * + ? { } [ ] \ | ( )")
+
+im = Image.open('/home/sputnik-9/Documents/JuanJoseDuranRincon/python/SKILLS REVIEW|introduccion/regex.png')
+im.show()
